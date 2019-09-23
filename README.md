1065bef0153341e3dc10573a2c30c50b09a44c4b
Avinash V Pillai <37207365+avinashvpillai@users.noreply.github.com>
2019-09-23 23:23:06 +0530

    Add files via upload
    property loading with global access within same package
    re-loadable feature.
    replace 1st line data

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ go property File Handler
 * Property file loader support:
 go get -u github.com/magiconair/properties
 https://github.com/magiconair/properties#updated-git-tags
+
+
+go get -u github.com/spf13/viper
--- main.go
+++ main.go
@@ -2,14 +2,50 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/magiconair/properties"
 )
 
 var propertyFiles = []string{"./application.properties"}
+var props = properties.MustLoadFiles(propertyFiles, properties.UTF8, true)
 
 func main() {
-	p := properties.MustLoadFiles(propertyFiles, properties.UTF8, true)
-	var val = p.GetString("state", "default")
+	var val = getProp("state")
 	fmt.Println("property value for state : ", val)
+
+	saveDataToProperties()
+
+	val = getProp("state")
+	fmt.Println("new property value for state : ", val)
+}
+
+func reloadProperties() {
+	fmt.Println("reloading properties..")
+	props = properties.MustLoadFiles(propertyFiles, properties.UTF8, true)
+}
+
+func getProp(key string) string {
+	value := ""
+	var ok bool
+	value, ok = props.Get(key)
+	if !ok {
+		return ""
+	}
+	return value
+}
+
+func saveDataToProperties() {
+	file, err := os.OpenFile("application.properties", os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+	defer file.Close()
+	_, err = file.WriteAt([]byte("state=Kerala"), 0) // Write at 0 beginning
+	if err != nil {
+		log.Fatalf("failed writing to file: %s", err)
+	}
+	reloadProperties()
+
 }
